84b2566ad923e62ff91246095d4f1c6c43dd663d
Dan Kegel <dkegel@fastly.com>
2022-05-04 07:50:18 -0700

    run.sh, score.py: add -k option to set precision of output floats

--- score.py
+++ score.py
@@ -7,11 +7,12 @@ class WAFEfficacy:
     """
     Calculates WAF Efficacy Score
     """
-    def __init__(self, filename: str, waf_response: str, attack_types: List[str]) -> None:
+    def __init__(self, filename: str, waf_response: str, attack_types: List[str], precision: int) -> None:
         with open(filename, 'r') as f:
             self.results = [json.loads(line) for line in f]
         self.attack_types = attack_types if attack_types else ['cmdexe', 'sqli', 'traversal', 'xss']
         self.waf_response = waf_response if waf_response else "406 Not Acceptable"
+        self.precision = precision if precision else 1
     
     def __true_positives_false_negatives(self, attack_type: str) -> Tuple[int, int]:
         true_positives = 0
@@ -43,6 +44,8 @@ class WAFEfficacy:
         true_negatives = 0
         false_positives = 0
 
+        fmt = "{0:." + str(self.precision) + "f}%"
+
         for attack_type in self.attack_types:
             tp, fn = self.__true_positives_false_negatives(attack_type)
             tn, fp = self.__true_negatives_false_positives(attack_type)
@@ -58,13 +61,13 @@ class WAFEfficacy:
             sensitivity = tp / (tp + fn)
             specificity = tn / (tn + fp)
             balanced_accuracy = (sensitivity + specificity) / 2
-            print("Efficacy", "{0:.1f}%".format(balanced_accuracy * 100))
+            print("Efficacy", fmt.format(balanced_accuracy * 100))
         
         print("------------- WAF Efficacy -------------" )
         sensitivity = true_positives / (true_positives + false_negatives)
         specificity = true_negatives / (true_negatives + false_positives)
         balanced_accuracy = (sensitivity + specificity) / 2
-        print("{0:.1f}%".format(balanced_accuracy * 100))
+        print(fmt.format(balanced_accuracy * 100))
 
 
 def main() -> None:
@@ -72,9 +75,10 @@ def main() -> None:
     parser.add_argument('-f', '--filename', dest='filename', required=True, help='filename of json formatted waf efficacy results', type=str)
     parser.add_argument('-a', '--attack-types', dest='attack_types', required=False, help='list of one or more attack types', nargs='+', type=str)
     parser.add_argument('-r', '--waf-response', dest='waf_response', required=False, help='HTTP status code the WAF uses for blocking requests', type=str)
+    parser.add_argument('-k', '--precision', dest='precision', required=False, default=1, help='number of decimal places in percentages', type=int)
     args = parser.parse_args()
     
-    waf_efficacy = WAFEfficacy(args.filename, args.waf_response, args.attack_types)
+    waf_efficacy = WAFEfficacy(args.filename, args.waf_response, args.attack_types, args.precision)
     waf_efficacy.score()
 
 if __name__ == '__main__':
